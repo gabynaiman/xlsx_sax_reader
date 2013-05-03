bbc31aeb488df39beda6cbbdd3424a8225713350
Gabriel Naiman <gabynaiman@gmail.com>
2013-05-03 09:42:37 -0300

    Extract excel file system

---
+++ lib/xlsx_sax_reader/file_system.rb
@@ -0,0 +1,34 @@
+module XlsxSaxReader
+  class FileSystem
+
+    def self.open(filename)
+      begin
+        file_system = self.new(filename)
+        yield file_system
+      ensure
+        file_system.close
+      end
+    end
+
+    def initialize(filename)
+      @zip = Zip::ZipFile.open filename
+    end
+
+    def close
+      @zip.close
+    end
+
+    def workbook
+      @zip.read('xl/workbook.xml').match(/<sheets>.*<\/sheets>/).to_s
+    end
+
+    def shared_strings
+      @zip.read('xl/sharedStrings.xml')
+    end
+
+    def sheets
+      @zip.glob('xl/worksheets/sheet*.xml').sort.map{ |f| @zip.read(f).match(/<sheetData>.*<\/sheetData>/).to_s }
+    end
+
+  end
+end
